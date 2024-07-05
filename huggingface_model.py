50a62f6c78e882b66ba07bfa67ddd5c0444c10b9
Richard Kelley <richard.kelley@gmail.com>
2024-07-05 14:25:23 -0700

    Fixed typo, added generate_until and loglikelihood.

--- huggingface_model.py
+++ huggingface_model.py
@@ -6,11 +6,14 @@ import torch.nn.functional as F
 import transformers
 from transformers import PreTrainedModel, PreTrainedTokenizer, AutoTokenizer
 
+import copy
+
 from utils import (
     get_rolling_token_windows,
     make_disjoint_window,
     Collator,
-    pad_and_concat
+    pad_and_concat,
+    stop_sequences_criteria
 )
 
 from typing import List, Tuple, Union, Optional
@@ -35,6 +38,7 @@ class HFLM(LM):
                 str, PreTrainedTokenizer
             ]
         ] = None,
+        truncation : Optional[bool] = False,
         device: Optional[str] = "cuda",
         dtype: Optional[Union[str, torch.dtype]] = "auto", # NB: MLX incompatibility
         add_bos_token: Optional[bool] = False, # Need for Gemma-2
@@ -69,6 +73,7 @@ class HFLM(LM):
         self.add_bos_token = add_bos_token
         self.custom_prefix_token_id = prefix_token_id
         self.batch_size_per_gpu = batch_size
+        self.truncation = truncation
 
         # get backend
         self._get_backend()
@@ -195,8 +200,44 @@ class HFLM(LM):
 
         return encoding
 
-    def loglikelihood(self, requests) -> List[Tuple[float, bool]]:
-        pass
+    def _encode_pair(self, context, continuation):
+        n_spaces = len(context) - len(context.rstrip())
+
+        if n_spaces > 0:
+            continuation = context[-n_spaces:] + continuation
+            context = context[:-n_spaces]
+
+        model_class = getattr(self, "AUTO_MODEL_CLASS", None)
+
+        if model_class == transformers.AutoModelForSeq2SeqLM:
+            context_enc = self.tok_encode(context)
+            continuation_enc = self.tok_encode(continuation, add_special_tokens=False)
+        else:
+            whole_enc = self.tok_encode(context + continuation)
+            context_enc = self.tok_encode(context)
+
+            context_enc_len = len(context_enc)
+            continuation_enc = whole_enc[context_enc_len:]
+
+        return context_enc, continuation_enc
+
+    def loglikelihood(self, requests: List[Tuple[str,str]]) -> List[Tuple[float, bool]]:
+        '''
+        requests is a list of (context, continuation) pairs
+        '''
+        new_reqs = []
+        for context, continuation in requests:
+            if context == '':
+                context_enc, continuation_enc = (
+                    [self.prefix_token_id],
+                    self.tok_encode(continuation)
+                )
+            else:
+                context_enc, continuation_enc = self._encode_pair(context, continuation)
+
+            new_reqs.append(((context, continuation), context_enc, continuation_enc))
+
+        return self._loglikelihood_tokens(new_reqs)
 
     def _model_call(self, inps, attn_mask=None, labels=None):
         with torch.no_grad():
@@ -309,9 +350,8 @@ class HFLM(LM):
                         answer = (float(logits.sum()), bool(max_equal))
                         res.append(answer)
 
-            return re_ord.get_original(res)
+        return re_ord.get_original(res)
             
-
     def loglikelihood_rolling(self, requests : List[str]) -> List[Tuple[float]]:
         '''
         We will assume that `requests` has type List[str] for this implementation
@@ -345,6 +385,157 @@ class HFLM(LM):
 
         return loglikelihoods
         
-    def generate_until(self, requests) -> List[str]:
-        pass
+    def tok_decode(self, tokens, skip_special_tokens=True):
+        return self.tokenizer.decode(tokens, skip_special_tokens=skip_special_tokens)
+
+    @property
+    def eot_token_id(self):
+        return self.tokenizer.eos_token_id
     
+    @property
+    def max_gen_toks(self) -> int:
+        return 256
+    
+    def tok_batch_encode(
+        self,
+        strings: List[str],
+        padding_side: str = "left",
+        left_truncate_len: int = None,
+        truncation: bool = False,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        old_padding_side = self.tokenizer.padding_side
+        self.tokenizer.padding_side = padding_side
+
+        add_special_tokens = {}
+        if self.AUTO_MODEL_CLASS == transformers.AutoModelForCausalLM:
+            add_special_tokens = {"add_special_tokens": False or self.add_bos_token}
+
+        encoding = self.tokenizer(
+            strings,
+            truncation=truncation,
+            padding="longest",
+            return_tensors="pt",
+            **add_special_tokens,
+        )
+        if left_truncate_len is not None:
+            encoding["input_ids"] = encoding["input_ids"][:, -left_truncate_len:]
+            encoding["attention_mask"] = encoding["attention_mask"][
+                :, -left_truncate_len:
+            ]
+        self.tokenizer.padding_side = old_padding_side
+
+        return encoding["input_ids"], encoding["attention_mask"]
+
+    def _model_generate(self, context, max_length, stop, **generation_kwargs):
+        generation_kwargs["temperature"] = generation_kwargs.get("temperature", 0.0)
+        do_sample = generation_kwargs.get("do_sample", None)
+
+        if generation_kwargs.get("temperature") == 0.0 and do_sample is None:
+            generation_kwargs["do_sample"] = do_sample = False
+
+        if do_sample is False and generation_kwargs.get("temperature") == 0.0:
+            generation_kwargs.pop("temperature")
+
+        stopping_criteria = stop_sequences_criteria(
+            self.tokenizer, stop, context.shape[1], context.shape[0]
+        )
+
+        return self.model.generate(
+            input_ids = context,
+            max_length = max_length,
+            stopping_criteria = stopping_criteria,
+            pad_token_id = self.tokenizer.pad_token_id,
+            use_cache = True,
+            **generation_kwargs
+        )
+
+    def generate_until(self, requests) -> List[str]:
+        res = []
+
+        def _collate(req: Tuple[str, dict]):
+            """Defines the key for the sorted method"""
+            # the negative sign on len(toks) sorts descending - this has a few advantages:
+            # - time estimates will always be over not underestimates, which is more useful for planning
+            # - to know the size of a batch when going through the list, you know the first one is always the batch
+            #   padded context length. this is useful to simplify the batching logic and more importantly to make
+            #   automatic adaptive batches much much easier to implement
+            # - any OOMs will happen right away rather than near the end
+            toks = self.tok_encode(req[0])
+            return -len(toks), req[0]
+
+        batch_size = self.batch_size
+
+        re_ords = Collator(
+            requests,
+            sort_fn=_collate,
+            group_by="gen_kwargs",
+            group_fn=lambda x: x[1],
+        )
+
+        chunks = re_ords.get_batched(n=batch_size, batch_fn=None)
+        for chunk in chunks:
+            contexts, all_gen_kwargs = zip(*chunk)
+            gen_kwargs = all_gen_kwargs[0]
+            until = None
+
+            if isinstance(gen_kwargs, dict):
+                kwargs = copy.deepcopy(gen_kwargs)
+                if "until" in kwargs.keys():
+                    until = kwargs.pop("until")
+                    if isinstance(until, str):
+                        until = [until]
+                    elif not isinstance(until, list):
+                        raise ValueError(
+                            f"Expected `kwargs['until']` to be of type Union[str,list] but got {until}"
+                        )
+            else:
+                raise ValueError(
+                    f"Expected `kwargs` to be of type `dict` but got {type(gen_kwargs)}"
+                )
+            
+            eos = self.tok_decode(self.eot_token_id, skip_special_tokens=False)
+            if not until:
+                until = [eos]
+            else:
+                until.append(eos)
+
+            if "max_gen_toks" in kwargs.keys():
+                max_gen_toks = kwargs.pop("max_gen_toks")
+            else:
+                max_gen_toks = self.max_gen_toks
+
+            if self.AUTO_MODEL_CLASS == transformers.AutoModelForCausalLM:
+                max_ctx_len = self.max_length - max_gen_toks
+            elif self.AUTO_MODEL_CLASS == transformers.AutoModelForSeq2SeqLM:
+                max_ctx_len = self.max_length
+
+            context_enc, attn_masks = self.tok_batch_encode(
+                contexts, left_truncate_len=max_ctx_len, truncation=self.truncation
+            )
+            context_enc = context_enc.to(self.device)
+            attn_masks = attn_masks.to(self.device)
+
+            if "max_length" not in kwargs:
+                kwargs["max_length"] = context_enc.shape[1] + max_gen_toks
+
+            cont = self._model_generate(
+                context=context_enc,
+                attention_mask=attn_masks,
+                stop=until,
+                **kwargs,
+            )
+
+            cont_toks_list = cont.tolist()
+            for cont_toks, context in zip(cont_toks_list, contexts):
+                if self.AUTO_MODEL_CLASS == transformers.AutoModelForCausalLM:
+                    cont_toks = cont_toks[context_enc.shape[1] : ]
+
+                s = self.tok_decode(cont_toks)
+
+                for term in until:
+                    if len(term) > 0:
+                        s = s.split(term)[0]
+
+                res.append(s)
+        res = re_ords.get_original(res)
+        return res
