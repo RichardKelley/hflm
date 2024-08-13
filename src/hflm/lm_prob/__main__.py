86f868f8f6d603d50327ad319104836c8abbc0c4
Duncan Wilson <duncanw@nevada.unr.edu>
2024-08-13 14:20:14 -0700

    add the scripts

---
+++ src/hflm/lm_prob/__main__.py
@@ -0,0 +1,32 @@
+import argparse
+from hflm.huggingface_model import HFLM
+import logging
+import sys
+import warnings
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", type=str, help="")
+    parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--string", "-s", type=str, help="string to calculate log probs of")
+    return parser
+
+def lm_prob():
+    parser = setup_parser()
+    args = parser.parse_args()
+    
+    if args.model is None:
+        logging.error("Model name required.")
+        sys.exit(1)
+
+    if args.string is None:
+        logging.error("String required.")
+        sys.exit(1)
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        model = HFLM(model=args.model, device=args.device)
+        print(model.loglikelihood_rolling([args.string], disable_tqdm=True)[0])
+
+if __name__ == "__main__":
+    lm_prob()
